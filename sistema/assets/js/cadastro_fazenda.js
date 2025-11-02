05c6cb4ed34baf069e1649c76124f86b304f4205
Guilherme Dias Ferreira <162358873+guidiasferreira@users.noreply.github.com>
2025-11-02 20:20:12 -0300

    Delete sistema directory

--- sistema/assets/js/cadastro_fazenda.js
+++
@@ -1,8 +0,0 @@
-const radioNovaCultura = document.querySelector('#nova_cultura');
-const radioCulturaExistente = document.querySelector('#cultura_existente');
-const selectInput = document.querySelector('.select');
-
-
-radioNovaCultura.addEventListener('click', () => {
-    selectInput.classList.toggle("displayNone");}
-);
